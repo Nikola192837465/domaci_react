ddcd1da3c00c1ba3afbc6c1a86f935dc7deef251
Nikola192837465 <fnikola71@gmail.com>
2024-09-08 09:11:47 +0200

    Kreirana reusable komponenta Title i implementacija dizajna pocetne stranice

--- react-domaci/src/components/layout/stranice/KontaktiAplikacija.jsx
+++ react-domaci/src/components/layout/stranice/KontaktiAplikacija.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import Title from "../../Title";
 
 export default function KontaktiAplikacija() {
-  return <div>KontaktiAplikacija</div>;
+  //fokus na polju za pretragu odmah pri ucitavanju stranice
+  let inputSearch = useRef(null);
+  useEffect(() => {
+    inputSearch.current.focus();
+  });
+
+  return (
+    <div>
+      <Title classes={"title"} text={"Kontakti"} />
+      <Title classes={"subtitle"} text={"aplikacija"} />
+
+      <input
+        type="text"
+        placeholder="Kljuc za pretragu kontakata..."
+        className="mb-2"
+        style={{ padding: ".5rem 7rem" }}
+        ref={inputSearch}
+      />
+
+      <section
+        className="d-flex"
+        style={{ gap: 15, maxWidth: 1600, margin: "auto" }}
+      ></section>
+    </div>
+  );
 }
