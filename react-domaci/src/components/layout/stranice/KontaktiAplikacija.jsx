d6fe4ffe488bdaba75a2622d7215c3d0459aa67b
Nikola192837465 <fnikola71@gmail.com>
2024-09-12 11:57:01 +0200

    Ispravka bagova

--- react-domaci/src/components/layout/stranice/KontaktiAplikacija.jsx
+++ react-domaci/src/components/layout/stranice/KontaktiAplikacija.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Title from "../../Title";
 
 import Data from "../../../Data.json";
@@ -11,6 +11,8 @@ export default function KontaktiAplikacija() {
     inputSearch.current.focus();
   });
 
+  const [pretragaKontakta, setPretragaKontakta] = useState("");
+
   return (
     <div>
       <Title classes={"title"} text={"Kontakti"} />
@@ -22,13 +24,24 @@ export default function KontaktiAplikacija() {
         className="mb-2"
         style={{ padding: ".5rem 7rem" }}
         ref={inputSearch}
+        onChange={(e) => setPretragaKontakta(e.target.value)}
       />
 
       <section
         className="d-flex text-left"
         style={{ gap: 15, maxWidth: 1600, margin: "auto", flexWrap: "wrap" }}
       >
-        {Data.map((kontakt) => (
+        {Data.filter((kontakt) => {
+          if (pretragaKontakta === "") {
+            return kontakt;
+          } else if (
+            kontakt.first_name
+              .toLocaleLowerCase()
+              .includes(pretragaKontakta.toLocaleLowerCase())
+          ) {
+            return kontakt;
+          }
+        }).map((kontakt) => (
           <Kontakt kontakt={kontakt} />
         ))}
       </section>
